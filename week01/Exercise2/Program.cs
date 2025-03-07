4f404ca2f52c5c469511fb6cbff4b4d1c3afde9c
Tony <anthonyv88803@gmail.com>
2025-03-07 16:05:18 -0700

    Your commit message

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade in a percentage?");
+        string input = Console.ReadLine();
+
+        if (float.TryParse(input, out float grade))
+        {
+            if (grade >= 90)
+            {
+                Console.WriteLine($"You got a {grade}% so you get an A.");
+            }
+            else if (grade >= 80)
+            {
+                Console.WriteLine($"You got a {grade}% so you get a B.");
+            }
+            else if (grade >= 70)
+            {
+                Console.WriteLine($"You got a {grade}% so you get a C.");
+            }
+            else if (grade >= 60)
+            {
+                Console.WriteLine($"You got a {grade}% so you get a D.");
+            }
+            else
+            {
+                Console.WriteLine($"You got a grade below 60% so you get an F.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a numeric grade.");
+        }
     }
 }
