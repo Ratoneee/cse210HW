c5e190b6164221353674119a7f2c7f050687fa0f
Tony <anthonyv88803@gmail.com>
2025-03-07 16:15:00 -0700

    Your commit message

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,44 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        string playagain;
+        
+        do
+        {
+            // picks random number 1-10
+            int magicNumber = randomGenerator.Next(1, 10);
+            int guess;
+            int attempts = 0;
+            
+            Console.WriteLine("welcome to guess the magic number!");
+            
+            do
+            {
+                Console.Write("what is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+                
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! It took you {attempts} attempts.");
+                }
+            } while (guess != magicNumber);
+            
+            Console.Write("Do you want to play again? (yes/no): ");
+            playagain = Console.ReadLine().ToLower();
+        } while (playagain == "yes");
+        
+        Console.WriteLine("Thanks you for playing!");
     }
 }
