c0916143b6f4411f41a13c4799756b0c6dc375f4
Tony <anthonyv88803@gmail.com>
2025-03-18 12:17:01 -0700

    added all the W03 Learning Activity work including 'fraction.cs' and updating the 'program.cs'

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,69 @@
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _numerator = wholeNumber;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        if (denominator == 0)
+        {
+            Console.WriteLine("Error: Denominator cannot be zero.");
+            _numerator = 1;
+            _denominator = 1;
+        }
+        else
+        {
+            _numerator = numerator;
+            _denominator = denominator;
+        }
+    }
+
+    public int GetNumerator()
+    {
+        return _numerator;
+    }
+
+    public void SetNumerator(int numerator)
+    {
+        _numerator = numerator;
+    }
+
+    public int GetDenominator()
+    {
+        return _denominator;
+    }
+
+    public void SetDenominator(int denominator)
+    {
+        if (denominator == 0)
+        {
+            Console.WriteLine("Error: Denominator cannot be zero.");
+        }
+        else
+        {
+            _denominator = denominator;
+        }
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_numerator}/{_denominator}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_numerator / _denominator;
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -2,8 +2,26 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        f3.SetNumerator(7);
+        f3.SetDenominator(2);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 0);
+        Console.WriteLine(f4.GetFractionString());
     }
 }
